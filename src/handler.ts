64a577638e09d74ecbb3c48fdb3ee5bbc370de83
Petr <petr@flaks.dev>
2021-10-10 21:06:18 +0300

    feat: use caching

--- src/handler.ts
+++ src/handler.ts
@@ -1,42 +1,42 @@
 export async function handleRequest(request: Request): Promise<Response> {
-  const url = new URL(request.url);
+  const requestUrl = new URL(request.url);
 
-  if (!url.searchParams.has('domain')) {
-    return new Response(JSON.stringify({
-      type: 'validation_error',
-      detail: 'Missing required parameter: "domain".'
-    }), {
-      status: 400,
-    });
-  }
+    if (!requestUrl.searchParams.has('domain')) {
+      return new Response(JSON.stringify({
+        type: 'validation_error',
+        detail: 'Missing required parameter: "domain".'
+      }), {
+        status: 400,
+      });
+    }
 
-  const domain = url.searchParams.get('domain');
+    const domain = requestUrl.searchParams.get('domain');
 
-  let faviconBaseUrl: URL;
+    let faviconBaseUrl: URL;
 
-  try {
-    faviconBaseUrl = new URL(`https://${domain}`);
-  } catch (_) {
-    return new Response(JSON.stringify({
-      type: 'validation_error',
-      detail: 'Parameter "domain" contains invalid domain name.'
-    }), {
-      status: 400,
-    });
-  }
+    try {
+      faviconBaseUrl = new URL(`https://${domain}`);
+    } catch (_) {
+      return new Response(JSON.stringify({
+        type: 'validation_error',
+        detail: 'Parameter "domain" contains invalid domain name.'
+      }), {
+        status: 400,
+      });
+    }
 
-  let response: Response;
+    let faviconResponse: Response;
 
-  try {
-    response = await fetch(`${faviconBaseUrl.toString()}/favicon.ico`);
-  } catch (_) {
-    return new Response(JSON.stringify({
-      type: 'favicon_fetching_error',
-      detail: 'Could not to fetch a favicon for unknown network-related reason.'
-    }), {
-      status: 502,
-    });
-  }
+    try {
+      faviconResponse = await fetch(`${faviconBaseUrl.toString()}/favicon.ico`);
+    } catch (_) {
+      return new Response(JSON.stringify({
+        type: 'favicon_fetching_error',
+        detail: 'Could not to fetch a favicon for unknown network-related reason.'
+      }), {
+        status: 502,
+      });
+    }
 
-  return new Response(await response.blob())
+    return new Response(await faviconResponse.blob());
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,30 @@
 import { handleRequest } from './handler'
 
 addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request))
+  event.respondWith(handleCacheableRequest(event))
 })
+
+const handleCacheableRequest = async (event: FetchEvent) => {
+  const requestUrl = new URL(event.request.url);
+
+  // Construct the cache key from the cache URL
+  const cacheKey = new Request(requestUrl.toString(), event.request)
+  const cache = caches.default
+
+  // Check whether the value is already available in the cache
+  // if not, you will need to fetch it from origin, and store it in the cache
+  // for future access
+  let cachedResponse = await cache.match(cacheKey)
+
+  if (cachedResponse) {
+    return cachedResponse;
+  }
+
+  const response = await handleRequest(event.request);
+
+  response.headers.append("Cache-Control", "public, max-age=86400")
+
+  event.waitUntil(cache.put(cacheKey, response.clone()));
+
+  return response;
+}
