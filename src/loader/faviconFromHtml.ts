28ef9d5a2a354708d96f5f0dfefc3ccc64561c1c
Petr <petr@flaks.dev>
2022-07-03 12:45:42 +0300

    fix: process `sizes` attribute correctly
    Closes #6.

--- src/loader/faviconFromHtml.ts
+++ src/loader/faviconFromHtml.ts
@@ -55,7 +55,8 @@ export const getLargestFaviconFromFromHtml = async (domain: string): Promise<Blo
   return undefined;
 };
 
-type Favicon = { mimeType: string | undefined; size: { height: number; width: number } | undefined; url: string };
+type Resolution = { height: number; width: number };
+type Favicon = { mimeType: string | undefined; size: Resolution | undefined; url: string };
 
 const extractIcons = async (response: Response): Promise<Array<Favicon>> => {
   const root = parse(await response.text());
@@ -71,44 +72,82 @@ const extractIcons = async (response: Response): Promise<Array<Favicon>> => {
         return undefined;
       }
 
-      let resolution = undefined;
+      const absoluteHref = normalizeHref(response.url, href);
 
-      if (sizes !== undefined) {
-        const [widthString, heightString] = sizes.trim().split('x');
-
-        const width = widthString === undefined ? NaN : parseInt(widthString, 10);
-        const height = heightString === undefined ? NaN : parseInt(heightString, 10);
-
-        if (!isNaN(width) && !isNaN(height)) {
-          resolution = {
-            width: width,
-            height: height,
-          };
-        }
-      }
-
-      let absoluteHref = href.trim();
-
-      // fix link if it starts with relative protocol (://)
-      if (absoluteHref.startsWith('://')) {
-        absoluteHref = 'https' + absoluteHref;
-      }
-
-      // fix link if its relative
-      if (!absoluteHref.startsWith('http://') && !absoluteHref.startsWith('https://')) {
-        const currentUrl = new URL(response.url);
-        currentUrl.pathname = absoluteHref;
-
-        absoluteHref = currentUrl.toString();
-      }
-
-      console.info(`Found icon with URL: ${absoluteHref} (type: ${type ?? '???'}, size: ${sizes ?? '???'})`);
+      console.info(`Found icon with URL: ${absoluteHref} (type: ${type ?? '???'}, sizes: ${sizes ?? '???'})`);
 
       return {
         url: absoluteHref,
         mimeType: type?.trim(),
-        size: resolution,
+        size: sizes !== undefined ? getResolutionFromSizesAttribute(sizes) : undefined,
       };
     })
     .filter((favicon): favicon is Favicon => favicon !== undefined);
+};
+
+/**
+ * @see https://html.spec.whatwg.org/multipage/links.html#rel-icon
+ */
+const getResolutionFromSizesAttribute = (sizes: string): Resolution | undefined => {
+  const resolutions = sizes
+    .trim()
+    .split(' ')
+    .map((sizeString): Resolution | undefined => {
+      if (sizeString === 'any') {
+        return {
+          width: Infinity,
+          height: Infinity,
+        };
+      }
+
+      // different cases may be used: e.g. 16x16 and 16X16
+      const [widthString, heightString] = sizeString.split(/x/i, 2);
+
+      if (widthString === undefined || heightString === undefined) {
+        return undefined;
+      }
+
+      // leading zeroes are not allowed according to the spec
+      if (widthString.startsWith('0') || heightString.startsWith('0')) {
+        return undefined;
+      }
+
+      const width = parseInt(widthString, 10);
+      const height = parseInt(heightString, 10);
+
+      if (isNaN(width) || isNaN(height)) {
+        return undefined;
+      }
+
+      return {
+        width: width,
+        height: height,
+      };
+    })
+    .filter((resolution): resolution is Resolution => resolution !== undefined)
+    .sort(
+      (resolutionA, resolutionB) =>
+        Math.max(resolutionB.width, resolutionB.height) - Math.max(resolutionA.width, resolutionA.height),
+    );
+
+  return resolutions[0];
+};
+
+const normalizeHref = (originUrl: string, href: string): string => {
+  let absoluteHref = href.trim();
+
+  // fix link if it starts with relative protocol (://)
+  if (absoluteHref.startsWith('://')) {
+    absoluteHref = 'https' + absoluteHref;
+  }
+
+  // fix link if its relative
+  if (!absoluteHref.startsWith('http://') && !absoluteHref.startsWith('https://')) {
+    const currentUrl = new URL(originUrl);
+    currentUrl.pathname = absoluteHref;
+
+    absoluteHref = currentUrl.toString();
+  }
+
+  return absoluteHref;
 };
