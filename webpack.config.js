01bee0f27ede2bcec8fd856f2f4aeb31eef3d1ed
Petr <petr@flaks.dev>
2022-07-10 17:07:02 +0300

    build: use target=webworker

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'worker.ts'),
+  target: 'webworker',
   output: {
     filename: 'worker.js',
     path: path.resolve(__dirname, 'dist'),
